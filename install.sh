16843a8c176bc4c40539e779a384d051c980bdb3
Connor Kuziemko <150721651+FancyFeast666@users.noreply.github.com>
2025-04-16 10:58:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if ! command -v "npm" 2>&1 >/dev/null; then
-  npm install
+  npm install -y
 fi
 
 chmod u+x deploy-swarm.sh
@@ -11,11 +11,22 @@ sudo git config --system --add safe.directory /home/connorkuziemko2021/unix-fina
 npm install react-router-dom
 
 if ! command -v "docker" 2>&1 >/dev/null; then
-  apt install docker
+  apt install docker -y
 fi
 
 if ! command -v "docker-compose" 2>&1 >/dev/null; then
-  apt install docker-compose
+  apt install docker-compose -y
+fi
+
+if ! command -v "ufw" 2>&1 >/dev/null; then
+  apt install ufw -y
+  ufw allow OpenSSH
+  ufw allow http
+  ufw allow https
+  ufw enable
+  ufw reload
 fi
 
 ./deploy-swarm.sh
+
+
