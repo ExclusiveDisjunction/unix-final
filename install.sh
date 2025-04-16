a6adba03b367bfc04f17b726dcb7e55ece5aa43d
Connor Kuziemko <150721651+FancyFeast666@users.noreply.github.com>
2025-04-16 12:02:47 -0400

    Update install.sh
    finalizing firewall setup and testing cron job

--- install.sh
+++ install.sh
@@ -23,10 +23,20 @@ if ! command -v "ufw" 2>&1 >/dev/null; then
   ufw allow OpenSSH
   ufw allow http
   ufw allow https
-  ufw enable
+  echo "y" | ufw enable
   ufw reload
 fi
 
 ./deploy-swarm.sh
 
+echo "Do you want deploy-swarm added through cron {every 10 minutes} (y/n)"
+read -r ADD_CRON
+if [[ "$ADD_CRON" == "y" ]]; then
+  echo "🛠 Adding cron job as root..."
+  # Safely append to root's crontab without duplicating
+  (sudo crontab -l 2>/dev/null | grep -v "$SCRIPT"; echo "*/10 * * * * $(pwd)/$SCRIPT >> $(pwd)/deploy-swarm.log 2>&1") | sudo crontab -
+  echo "Cron job added to root: runs every 10 minutes"
+else
+  echo "Skipping cron setup."
+fi
 
