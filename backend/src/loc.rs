1f0de2ce3c4e5a80ca6882e78d321677d71a8e4c
exdisj <exdisj@outlook.com>
2025-04-16 14:41:37 -0400

    Added small extra functionality to the server

--- backend/src/loc.rs
+++ backend/src/loc.rs
@@ -1,10 +1,7 @@
 use std::path::PathBuf;
 
 pub const PROG_NAME: &str = "unix-back";
-#[cfg(debug_assertions)]
 pub const LOG_DIR: &str = "./logs";
-#[cfg(not(debug_assertions))]
-pub const LOG_DIR: &str = "/var/log/";
 
 pub fn make_log_path() -> PathBuf {
     let postfix = format!("{}-run-{}.log", PROG_NAME, chrono::Local::now());
