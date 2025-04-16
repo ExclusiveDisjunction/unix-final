63626cfe49382342681bbfbf90fe64ab66bc0e8b
Connor Kuziemko <150721651+FancyFeast666@users.noreply.github.com>
2025-04-16 09:38:22 -0400

    Update deploy-swarm.sh
    comments for script

--- deploy-swarm.sh
+++ deploy-swarm.sh
@@ -2,11 +2,14 @@
 
 cd /home/connorkuziemko2021/unix-final || exit
 
-git pull https://github.com/ExclusiveDisjunction/unix-final.git docker
+
+#if not using a github repo to manage the frontend, backend, and db comment out the next line, otherwise update with your github repo and branch
+git pull https://github.com/ExclusiveDisjunction/unix-final.git main
 
 DOCKER_BUILDKIT=1 docker build -t front ./frontend
 DOCKER_BUILDKIT=1 docker build -t back ./backend
 
-docker swarm init 2>/dev/null
 
+
+docker swarm init 2>/dev/null
 DOCKER_BUILDKIT=1 docker stack deploy -c compose.yaml test-stack
