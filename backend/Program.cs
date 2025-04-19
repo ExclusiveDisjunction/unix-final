e2f1410cd8054a060465cd55f3a3cb35eef44fc0
exdisj <exdisj@outlook.com>
2025-04-18 23:50:03 -0400

    Worked on requests, and migrated to use a file for secrets

--- backend/Program.cs
+++ backend/Program.cs
@@ -103,16 +103,3 @@ public class Program
         app.Run();
     }
 }
-
-record AddBookRequest
-{
-    public int Id { get; init; }
-    public required string Title { get; init; }
-}
-
-record AddGenreRequest
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public string? Description { get; set; }
-}
