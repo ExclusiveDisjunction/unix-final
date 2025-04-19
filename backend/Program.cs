c9a6c5cfcd975fb7e33da90e00c23ba51fd6a0f2
exdisj <exdisj@outlook.com>
2025-04-18 21:40:16 -0400

    Worked on database and program functionality

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,72 +1,104 @@
 using backend;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.ObjectPool;
 using Npgsql;
+
+namespace backend;
 
 public class Program
 {
-    public static void Main(string[] args)
+    private static WebApplication? app = null;
+
+    private static async void AddGenre(HttpContext context) 
     {
+        if (app is null) 
+        {
+            Console.Error.WriteLine("Application is not created.");
+            return;
+        }
+
+        app.Logger.LogInformation("Adding genre request");
+        try {
+            AddGenreRequest? info = await context.Request.ReadFromJsonAsync<AddGenreRequest>();
+            if (info is null) {
+                app.Logger.LogWarning("The provided logger is invalid");
+                return;
+            }
+
+            using var scope = app.Services.CreateScope();
+            var database = scope.ServiceProvider.GetRequiredService<Database>();
+
+            var genre = new Info.Genre(info.Id, info.Name, info.Description);
+            database.Genres.Add(genre);
+        }
+        catch (Exception ex) 
+        {
+            app.Logger.LogWarning($"Unable to parse request to add genre, error {ex}");
+        }
+    }
+    private static async void AddBook(HttpContext context)
+    {
+        if (app is null) 
+        {
+            Console.Error.WriteLine("Application is not created.");
+            return;
+        }
+
+        app.Logger.LogInformation("Adding book request");
+        try
+        {
+            AddBookRequest? info = await context.Request.ReadFromJsonAsync<AddBookRequest>();
+            if (info is null)
+            {
+                app.Logger.LogWarning("Book not found");
+                return;
+            }
+
+            /* using var scope = app.Services.CreateScope();
+            var database = scope.ServiceProvider.GetRequiredService<Database>();
+            database.Books.Add(new Info.Book(info.Id, info.Title));
+            */
+        } 
+        catch (Exception ex)
+        {
+            app.Logger.LogError($"Unable to parse book add request, error {ex.Message}");
+        }
+    }
+
+    private static WebApplicationBuilder? MakeBuilder(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
         builder.Services.AddDbContext<Database>();
         builder.Services.AddControllers();
 
-        var app = builder.Build();
+        return builder;
+    }
 
-// Configure the HTTP request pipeline.
+    public static void Main(string[] args)
+    {
+        var builder = MakeBuilder(args);
+        if (builder is null) {
+            Console.Error.WriteLine("Unable to create builder.");
+            return;
+        }
+
+        app = builder.Build();
+
+        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
         }
 
-//app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
 
-/*
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-*/
+        app.MapPost("/add-book/", Program.AddBook).WithName("AddBook");
 
-        app.MapPost("/add-book/", async context =>
-        {
-            app.Logger.LogInformation("Adding book request");
-            try
-            {
-                AddBookRequest? info = await context.Request.ReadFromJsonAsync<AddBookRequest>();
-                if (info is null)
-                {
-                    app.Logger.LogWarning("Book not found");
-                    return;
-                }
-
-                using var scope = app.Services.CreateScope();
-                var database = scope.ServiceProvider.GetRequiredService<Database>();
-                database.Books.Add(new backend.Info.Book(info.Id, info.Title));
-            }
-            catch (Exception ex)
-            {
-                app.Logger.LogError(ex.Message);
-            }
-        }).WithName("AddBook");
-
-        app.MapPost("/add-genre/", async context =>
-        {
-
-        }).WithName("AddGenre");
+        app.MapPost("/add-genre/", Program.AddGenre).WithName("AddGenre");
         
         app.Run();
     }
@@ -75,12 +107,12 @@ app.MapGet("/weatherforecast", () =>
 record AddBookRequest
 {
     public int Id { get; init; }
-    public string Title { get; init; }
+    public required string Title { get; init; }
 }
 
 record AddGenreRequest
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
 }
