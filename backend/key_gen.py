bce92718cfeb13126dd275245424cf5541937264
Connor Kuziemko <150721651+FancyFeast666@users.noreply.github.com>
2025-04-20 16:59:18 -0400

    Add files via upload
    key generator for jwt

---
+++ backend/key_gen.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def print_random_characters(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    f=open("jwt_key","w")
+    f.write(random_string)
+
+if __name__ == "__main__":
+    print_random_characters(256)
